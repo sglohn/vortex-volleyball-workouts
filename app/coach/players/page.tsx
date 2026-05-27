1736bc34582aff5d69c8ae1fdb68c64b6aa186e6
sglohn <sglohn@gmail.com>
2026-05-26 20:27:10 -0400

    Update page.tsx

--- app/coach/players/page.tsx
+++ app/coach/players/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import FeetInchesInput from '@/components/FeetInchesInput'
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import FeetInchesInput from '@/components/FeetInchesInput'
